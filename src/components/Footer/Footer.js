51de1dcadbefc443c9312807bd7a25d9d7c3a7ac
hayatkhan1196 <dev1scaleupally@gmail.com>
2023-01-10 01:07:59 +0500

    make folder structure

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,7 @@
+import React from "react";
+import styled from "./Foooter.module.scss";
+const Footer = () => {
+  return <div>footer</div>;
+};
+
+export default Footer;
---
+++ src/pages/Contact/Contact.js
@@ -0,0 +1,7 @@
+import React from "react";
+import styled from "./Contact.module.scss";
+const Contact = () => {
+  return <div>contact</div>;
+};
+
+export default Contact;
